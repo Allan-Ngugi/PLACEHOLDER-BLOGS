ec3d172030985d407eb4d52ce252d82de03bfdcb
waikau maina <ericmaina.dev@gmail.com>
2023-06-23 13:07:32 +0300

    removed useless columns from the user table

--- db/migrate/20230623082848_devise_token_auth_create_users.rb
+++ db/migrate/20230623082848_devise_token_auth_create_users.rb
@@ -29,9 +29,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
       ## User Info
-      t.string :name
-      t.string :nickname
-      t.string :image
+      t.string :username
       t.string :email
 
       ## Tokens
